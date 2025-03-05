1a3811607404bf0ff1f1f47d32c06609171dfc01
Brian <musyimib449@gmail.com>
2025-03-05 21:41:39 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dog");
+
+function mondayWork (activity = "go to the office"){
+return `This Monday, I will ${activity}.`;
+}  
+mondayWork("work from home"); 
+
+function wrapAdjective(emp="special"){
+    return `You are ${emp}`;
+}
+
+// function wrapAdjective(symbol="*"){
+//     return `You are${symbol}a hard worker${symbol}.`
+// }
+// console.log(wrapAdjective("*"))
+
+// function wrapAdjective(symbol="||"){
+//     return `You are${symbol}a dedicated programmer${symbol}.`
+// }
+// console.log(wrapAdjective("||"))
+
+function wrapAdjective(symbol = "*") {
+    return function(adjective = "special") {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
+
+
+const wrapWithStar = wrapAdjective("*");
+console.log(wrapWithStar("a hard worker")); 
+const wrapWithPipes = wrapAdjective("||");
+console.log(wrapWithPipes("a dedicated programmer")); 
